f990777732673563b295707e547c392505a682a7
hylescupidez <19YlescupidezHunterE@bprep.org>
2018-09-06 09:02:33 -0700

    made a pokemon so thats cool

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,7 @@
+public class Pikachu {
+    public static void main(String[] args){
+        System.out.println("(\\__/)" );
+        System.out.println("(*^.^)");
+        System.out.println("(_(\")(\")");
+    }
+}
