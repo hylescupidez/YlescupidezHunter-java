cb5b2e0698f29e993dceb57ae454db71adbb6ce3
hylescupidez <19YlescupidezHunterE@bprep.org>
2018-09-10 09:20:07 -0700

    Box

---
+++ untitled/src/drawBox.java
@@ -0,0 +1,22 @@
+public class drawBox {
+    public static void main(String[] args) {
+        drawBox();
+        drawBox();
+        drawBox();
+        System.out.println("Complete");
+    }
+
+    public static void drawBox() {
+        drawTopBottom();
+        drawSide();
+        drawTopBottom();
+    }
+        public static void drawTopBottom() {
+            System.out.println("+-------+");
+            }
+        public static void drawSide() {
+            System.out.println("|       |");
+            System.out.println("|       |");}
+
+        }
+
