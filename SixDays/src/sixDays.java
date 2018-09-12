78ab43a2259e8a979a8a89cf24e959247286ac2f
hylescupidez <19YlescupidezHunterE@bprep.org>
2018-09-12 10:02:51 -0700

    Christmas thing

---
+++ SixDays/src/sixDays.java
@@ -0,0 +1,64 @@
+public class sixDays {
+    public static void main(String[] args){
+        firstVerse();
+        System.out.println();
+        secondVerse();
+        System.out.println();
+        thirdVerse();
+        System.out.println();
+        fourthVerse();
+        System.out.println();
+        fifthVerse();
+        System.out.println();
+        sixthVerse();
+
+
+
+
+
+
+
+    }
+    public static void firstVerse(){
+        System.out.println("On the 1st day of \"Xmas\", my true love sent to me");
+        firstGift();
+    }
+    public static void firstGift(){
+        System.out.println("a patridge in a pear tree");
+
+    }
+    public static void secondVerse(){
+        System.out.println("On the 2nd day of \"Xmas\", my true love sent to me");
+        secondGift();
+        firstGift();
+    }
+    public static void secondGift(){
+        System.out.println("two turtle doves, and");
+    }
+    public static void thirdVerse(){
+        System.out.println("On the 3rd day of \"Xmas\", my true love sent to me");
+        thirdGift();
+        secondGift();
+        firstGift();
+    }
+    public static void thirdGift(){
+        System.out.println("three French Hens,");
+    }
+    public static void fourthVerse(){
+        System.out.println("On the 4th day of \"Xmas\", my true love sent to me");
+        fourthGift();
+        thirdGift();
+        secondGift();
+        firstGift();
+    }
+    public static void fourthGift(){
+        System.out.println("four calling birds,");
+    }
+    public static void fifthVerse(){
+        System.out.println("On the 5th day of \"Xmas\", my true love sent to me");
+        fifthDay();
+    }
+    public static void fifthDay(){
+
+    }
+}
